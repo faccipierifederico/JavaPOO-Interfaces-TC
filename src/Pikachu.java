de729f615758df36973fad7553a354523f369bae
Federico Faccipieri <federicofaccipieri@gmail.com>
2023-08-29 10:14:48 -0300

    Excercise finished

--- src/Pikachu.java
+++ src/Pikachu.java
@@ -5,26 +5,26 @@ public class Pikachu extends Pokemon implements Electric{
 
     @Override
     public void thunder() {
-        System.out.println("I'm Bulbasur and I'm doing a thunder!");
+        System.out.println("I'm Pikachu and I'm doing a thunder!");
     }
 
     @Override
     public void thunderFist() {
-        System.out.println("I'm Bulbasur and I'm doing a thunder fist!");
+        System.out.println("I'm Pikachu and I'm doing a thunder fist!");
     }
 
     @Override
     protected void tackle() {
-        System.out.println("I'm Bulbasur and I'm doing a tackle!");
+        System.out.println("I'm Pikachu and I'm doing a tackle!");
     }
 
     @Override
     protected void scratch() {
-        System.out.println("I'm Bulbasur and I'm doing a scratch!");
+        System.out.println("I'm Pikachu and I'm doing a scratch!");
     }
 
     @Override
     protected void bite() {
-        System.out.println("I'm Bulbasur and I'm doing a bite!");
+        System.out.println("I'm Pikachu and I'm doing a bite!");
     }
 }
